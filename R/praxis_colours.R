47fa42a227a2355e1a9f1946c3bc618bcfeb03ba
mvainre <61190581+mvainre@users.noreply.github.com>
2020-06-08 12:47:19 +0300

    Official colours for Praxis

--- R/praxis_colours.R
+++ R/praxis_colours.R
@@ -1,9 +1,8 @@
 # The unofficial colours of Praxis
 praxis_colours <- c(
-  `blue`       = "#00599d",
-  `coral`      = "#ff6651",
-  `aqua`       = "#84dad3",
-  `yellow`     = "#ffd85d",
-  `light blue` = "#8cd9fc",
-  `green`      = "#bae18f",
-  `grey`       = "#d9d9d9")
+  `lightblue` = "#b2d2ec",
+  `blue`      = "#3F6ECD",
+  `red`       = "#F14D4D",
+  `black`     = "#000000",
+  `grey`       = "#A5A5A5",
+  `gold`      = "#C8A366")
--- R/praxis_palettes.R
+++ R/praxis_palettes.R
@@ -2,9 +2,9 @@
 #' @export
 
 praxis_palettes <- list(
-  `main`      = praxis_cols("blue", "coral", "aqua"),
+  `main`      = praxis_cols("lightblue", "blue", "red"),
   `binary`    = praxis_cols("blue", "grey"),
-  `cool`      = praxis_cols("blue", "aqua", "light blue"),
-  `traffic`   = praxis_cols("coral", "yellow", "green"),
-  `full`      = praxis_cols("blue", "coral", "aqua", "yellow", "light blue", "green", "grey")
+  `monochrome`= praxis_cols("black","grey"),
+  `cool`      = praxis_cols("lightblue", "blue", "grey"),
+  `full`      = praxis_cols("lightblue", "blue", "red", "black", "grey", "gold")
 )
